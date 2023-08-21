4553817f225be82970eaf40f78bbd7511de689dd
marcsiddiqui <marcsiddiqui@gmail.com>
2023-08-21 10:23:13 +0500

    first commit

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class FirstController extends Controller
+{
+    public function Welcome(){
+        $customer = [
+            ["Name" => "Arsalan", "Email" => "arsalan@gmail.com", "Coins" => 100],
+            ["Name" => "Asim", "Email" => "asim@gmail.com", "Coins" => 150],
+            ["Name" => "Nasir", "Email" => "nasir@gmail.com", "Coins" => 120],
+            ["Name" => "Jameel", "Email" => "jameel@gmail.com", "Coins" => 132],
+            ["Name" => "Sohail", "Email" => "sohail@gmail.com", "Coins" => 142]
+        ];
+    
+        return view('welcome', ["Customers" => $customer]);
+    }
+
+    public function Home(){
+        $customer = ["Name" => "Abdul Basit", "Age" => 10, "Count" => 100];
+        return view('home', $customer);
+    }
+
+    public function QueryParameter(){
+        $name = request("name");
+        $email = request("email");
+        $age = request("age");
+
+        $finalData = ["Name" => $name, "Email" => $email, "Age" => $age];
+
+        return view('queryparameter', $finalData);
+    }
+
+    public function RouteParameter($name, $age){
+        $finalData = ["Name" => $name, "Age" => $age];
+        return view('routeparameter', $finalData);
+    }
+
+    public function Products(){
+        
+        // $products = Product::all();
+        // $products = Product::orderby('name', 'asc')->get();
+        $products = Product::where('name', 'Tie')->get();
+    
+        return view('products', ["products" => $products]);
+    }
+
+    public function CreateProduct_get(){
+        return view("createproduct");
+    }
+
+    public function CreateProduct_post(){
+        $obj = new Product();
+        $obj->name = request("Name");
+        $obj->description = request("Description");
+        $obj->bundle_qunatity = request("BundleQuantity");
+        $obj->save();
+
+        return redirect("/products")->with("msg", "Added Successfully");
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\FirstController@Welcome');
+Route::get('/home', 'App\Http\Controllers\FirstController@Home');
+Route::get('/qp', 'App\Http\Controllers\FirstController@QueryParameter');
+Route::get('/rp/{name}/{age}', 'App\Http\Controllers\FirstController@RouteParameter');
+Route::get('/products', 'App\Http\Controllers\FirstController@Products');
+
+Route::get('/createproduct', 'App\Http\Controllers\FirstController@CreateProduct_get');
+Route::post('/createproduct', 'App\Http\Controllers\FirstController@CreateProduct_post');
